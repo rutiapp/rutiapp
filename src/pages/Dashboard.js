32a682fcb0cab0e1f9a3dfd0cf550520b47527f8
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2023-10-10 15:31:14 +0200

    Delete unused

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "../styles/dashboard.css";
 import "../styles/icons.css";
 import "../styles/svg.css";
 import Navbar from "../components/Navbar";
 import TopNavbar from "../components/TopNavBar";
-import { Outlet  } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 const Dashboard = () => {
   useEffect(() => {
   }, []);
   return (
     <div>
-  <div className="min-height-300 bg-primary position-absolute w-100" />
-  <Navbar/>
-  <main className="main-content position-relative border-radius-lg ">
-    {/* Navbar */}
-    <TopNavbar/>
-    {/* End Navbar */}
-    <Outlet />  
-  </main>
-</div>
+      <div className="min-height-300 bg-primary position-absolute w-100" />
+      <Navbar />
+      <main className="main-content position-relative border-radius-lg ">
+        {/* Navbar */}
+        <TopNavbar />
+        {/* End Navbar */}
+        <Outlet />
+      </main>
+    </div>
 
-)};
+  )
+};
 export default Dashboard;
